f75fd50449cea5e51a50645a6b58136f22d5ca2f
brianwachira <brianwachira7@gmail.com>
2019-01-24 21:49:32 +0300

    add About component import

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import Home from './components/shared/Home.vue'
+import About from './components/shared/About.vue'
 import Skills from './components/shared/Skills.vue'
 import myWork from './components/shared/MyWork.vue'
 import contactMe from './components/shared/ContactMe.vue'
