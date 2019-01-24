0ab3e3caf0fe6e5e864361b5730a99ef35a160e6
brianwachira <brianwachira7@gmail.com>
2019-01-24 20:36:21 +0300

    configure active class in navbar

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -8,18 +8,17 @@
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
                 <ul class="navbar-nav mr-auto">
-                  <li class="nav-item">
-                    <router-link to="/">Home</router-link>
-                  </li>
-                  <li class="nav-item">
-                    <router-link to="/skills">Skills</router-link>
-                  </li>
-                  <li class="nav-item">
-                    <router-link to="/work">My work</router-link>
-                  </li>
-                  <li class="nav-item">
-                    <router-link to="/contact">Contact me</router-link>
-                  </li>
+                    <router-link  to="/" tag="li" class="nav-item nav-link" active-class="active" exact><a>Home</a></router-link>
+                  
+
+                    <router-link to="/skills" tag="li" class="nav-item nav-link" active-class="active"><a>My skills</a></router-link>
+                  
+                  
+                    <router-link to="/work" tag="li" class="nav-item nav-link" active-class="active"><a>My work</a></router-link>
+                  
+                
+                    <router-link to="/contact" tag="li" class="nav-item nav-link" active-class="active"><a>Contact me</a></router-link>
+                  
                 </ul>
               </div>
             </div>
