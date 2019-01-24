134c5367487a1d01050ad4aa4d1f72428d58f337
brianwachira <brianwachira7@gmail.com>
2019-01-24 20:23:59 +0300

    place stmts in chronoligical order

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 import { routes } from './routes';
-import App from './App.vue'
-import { routes } from './routes';
 
 Vue.use(VueRouter);
 const router = new VueRouter({
