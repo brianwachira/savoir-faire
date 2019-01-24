65145ab05d2ac5a5927ba3c1c3461db3f86e3a7d
brianwachira <brianwachira7@gmail.com>
2019-01-24 23:05:52 +0300

    add main skills progress bars

--- src/components/shared/Skills.vue
+++ src/components/shared/Skills.vue
@@ -1,3 +1,34 @@
 <template>
-    <p>skills</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <div class="h2 heading">Tools I use mostly</div>
+                <label for="android">Android Application Development</label>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="android">HTML5</label>
+                <div class="progress">
+                    <div class="progress-bar bg-orange" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="android">CSS 3</label>
+                <div class="progress">
+                    <div class="progress-bar bg-cssblue" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="android">JQuery</label>
+                <div class="progress">
+                    <div class="progress-bar bg-jblue" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="android">Bootstrap</label>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="android">Vue.js</label>
+                <div class="progress">
+                    <div class="progress-bar bg-vue" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </template>
