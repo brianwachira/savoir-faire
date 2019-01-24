31df22a63433d0ab7a659fda753604d565208e94
brianwachira <brianwachira7@gmail.com>
2019-01-24 22:04:26 +0300

    add about me content

--- src/components/shared/About.vue
+++ src/components/shared/About.vue
@@ -1,3 +1,9 @@
 <template>
-    <p>aboufgbfgbfgbt</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col px-3 px-md-0 text-center text-light">
+                <p>You'll always find me going for unique trends,trying out different recipes in food..inshort always pursuing versatility. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit at architecto optio modi esse doloribus nostrum aliquid? Natus magni cum, dolorem impedit reprehenderit vitae a voluptate illo incidunt assumenda quisquam!</p>
+            </div>
+        </div>
+    </div>
 </template>
