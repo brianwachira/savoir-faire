c8b2ac5e0ae3d927bd8fced6995eae56f3e1bc86
brianwachira <brianwachira7@gmail.com>
2019-01-24 16:43:15 +0300

    add navbar & give it some border

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="background-img">
- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, dolor quisquam qui voluptas nemo saepe enim possimus eius sint? Animi eius deserunt magnam, autem dolor repudiandae quod totam dolorum consequuntur.</p>
+ <!--Navbar-->
+          <nav class="navbar navbar-expand-lg navbar-dark  nav-fill shadow-lg p-3 mb-5 rounded">
+            <div class="container">
+              <a class="navbar-brand" href="#"><strong>savoir-faire</strong></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Skills</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">My work</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact me</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <!-- Navbar -->
   </div>
 </template>
 
@@ -17,8 +42,15 @@ export default {
 
 <style>
 .background-img{
-  background-image: url('./assets/background3.2.jpg');
+  background-image: url('./assets/background2.jpg'); 
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  /*background: #659999;  /* fallback for old browsers */
+  /*background: -webkit-linear-gradient(to right, #f4791f, #659999);   Chrome 10-25, Safari 5.1-6 */
+  /*background: linear-gradient(to right, #f4791f, #659999); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
   height: 100%;
-  opacity: 0.97;
+  
 }
 </style>
