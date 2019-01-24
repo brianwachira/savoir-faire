d2106b2d93761323fb67d303c16a25ba550cd57d
brianwachira <brianwachira7@gmail.com>
2019-01-24 23:25:41 +0300

    change skills order of appearance

--- src/components/shared/Skills.vue
+++ src/components/shared/Skills.vue
@@ -1,5 +1,43 @@
 <template>
-    <p>skills</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 ">
+                <div class="h2 heading">Tools I use mostly</div>
+                <label for="android">Native Android Application Development</label>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="Vue.js">Vue</label>
+                <div class="progress">
+                    <div class="progress-bar bg-vue" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="Bootstrap">Bootstrap</label>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="HTML5">HTML5</label>
+                <div class="progress">
+                    <div class="progress-bar bg-orange" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="CSS 3">CSS 3</label>
+                <div class="progress">
+                    <div class="progress-bar bg-cssblue" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <label for="JQuery">JQuery</label>
+                <div class="progress">
+                    <div class="progress-bar bg-jblue" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <h2 class="header">Other Tools</h2>
+                <label for="C-programming">C programming</label>
+                <div class="progress">
+                    <div class="progress-bar bg-vue" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </template>
 <style scoped>
 .bg-orange{
