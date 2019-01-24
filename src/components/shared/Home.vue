5dbacf853b557bc9ae6b9162801ef5581a03ddf2
brianwachira <brianwachira7@gmail.com>
2019-01-24 21:51:38 +0300

    change button to router-link

--- src/components/shared/Home.vue
+++ src/components/shared/Home.vue
@@ -5,7 +5,7 @@
             <div class="col text-center mt-4">
                 <h1>Brian Wachira</h1>
                 <h2>Frontend/Android App Developer</h2>
-                <button class="btn btn-light mt-4">About Me</button>
+                <router-link class="btn btn-light mt-4" to="/about" tag="button">About Me</router-link>
             </div>
         </div>
     </div>
