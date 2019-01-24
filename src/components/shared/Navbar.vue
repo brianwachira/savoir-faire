fd32e9f52203d270a40a6057fc45ffffdcc804ce
brianwachira <brianwachira7@gmail.com>
2019-01-24 18:44:53 +0300

    make intial commit after refactoring navabr

---
+++ src/components/shared/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+ <!--Navbar-->
+          <nav class="navbar navbar-expand-lg navbar-dark  nav-fill shadow-lg p-3 mb-5 rounded">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"><strong>savoir-faire</strong></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Skills</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">My work</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact me</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <!-- Navbar -->
+</template>
