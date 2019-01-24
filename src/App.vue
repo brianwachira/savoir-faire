d226961ca0e2e9dae68245364d6543ced61c25a6
brianwachira <brianwachira7@gmail.com>
2019-01-24 17:12:53 +0300

    add home component and revert to uigradient background

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="background-img">
  <!--Navbar-->
           <nav class="navbar navbar-expand-lg navbar-dark  nav-fill shadow-lg p-3 mb-5 rounded">
-            <div class="container">
+            <div class="container-fluid">
               <a class="navbar-brand" href="#"><strong>savoir-faire</strong></a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -26,29 +26,30 @@
             </div>
           </nav>
           <!-- Navbar -->
+          <home></home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/shared/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
 .background-img{
-  background-image: url('./assets/background2.jpg'); 
+ /* background-image: url('./assets/background2.jpg'); 
   background-repeat: no-repeat;
   background-size: cover;
-  background-position: center center;
-  /*background: #659999;  /* fallback for old browsers */
-  /*background: -webkit-linear-gradient(to right, #f4791f, #659999);   Chrome 10-25, Safari 5.1-6 */
-  /*background: linear-gradient(to right, #f4791f, #659999); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background-position: center center;*/
+  background: #659999;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #f4791f, #659999);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #f4791f, #659999); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
   height: 100%;
   
