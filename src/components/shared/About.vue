f0dae7a6fc7b078224b286e1474519b544f32716
brianwachira <brianwachira7@gmail.com>
2019-01-25 18:58:20 +0300

    ...

--- src/components/shared/About.vue
+++ src/components/shared/About.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid"> 
         <div class="row">
-            <div class="col px-3 px-md-0 text-center text-light">
+            <div class="col px-3 px-md-0 text-center mt-5 text-light">
                 <p>You'll always find me going for unique trends,trying out different recipes in food..inshort always pursuing versatility. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit at architecto optio modi esse doloribus nostrum aliquid? Natus magni cum, dolorem impedit reprehenderit vitae a voluptate illo incidunt assumenda quisquam!</p>
             </div>
         </div>
