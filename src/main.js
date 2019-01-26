4fbf6e2ea64c0953722bb99979d85cd64735e622
brianwachira <brianwachira7@gmail.com>
2019-01-26 09:31:12 +0300

    import font awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import VueRouter from 'vue-router';
 import App from './App.vue'
 import { routes } from './routes';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
