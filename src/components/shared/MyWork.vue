75f1da44810a7c5234be8886e593248973450244
brianwachira <brianwachira7@gmail.com>
2019-01-26 17:31:39 +0300

    replace work carousel with card

--- src/components/shared/MyWork.vue
+++ src/components/shared/MyWork.vue
@@ -1,21 +1,41 @@
 <template>
-    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active align-items-sm-center">
-      <img src="../../assets/avuvi.png" class="d-block  w-100" alt="background1">
-    </div>
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+              <div class="card">
+                <img class="card-img-top" src="../../assets/avuvi.png" alt="background1">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <img class="card-img-top" src="../../assets/wahenga-walisema.png" alt="background2">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <img class="card-img-top" src="../../assets/flappybird.jpg" alt="background3">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
     <div class="carousel-item">
       <img src="../../assets/flappybird.jpg" class="d-block w-100" alt="background2">
     </div>
     <div class="carousel-item">
       <img src="../../assets/wahenga-walisema.png" class="d-block w-100" alt="background3">
-    </div>
-  </div>
+            </div>
+          </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
