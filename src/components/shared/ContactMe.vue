53bf07f7991144cc61109edcc1f2e15e58249e63
brianwachira <brianwachira7@gmail.com>
2019-01-26 12:15:06 +0300

    add custom icon class

--- src/components/shared/ContactMe.vue
+++ src/components/shared/ContactMe.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row"> 
             <div class="col-md-2 col-sm text-center"> 
-                <i class="fas fa-envelope" style="font-size: 3em; color: white;"></i>
+                <i class="fas fa-envelope icons fa-3x"></i>
                 <p class="text-light">brianwachira7@gmail.com</p>
             </div>
             <div class="col-md-2 col-sm text-center"> 
-                <i class="fab fa-whatsapp-square" style="font-size:3em; color:white;"></i>
+                <i class="fab fa-whatsapp-square icons fa-3x"></i>
                 <p class="text-light">+254707250844</p>
             </div>
             <div class="col-md-2 col-sm text-center">
                 <a href="https://www.github.com/brianwachira">
-                    <i class="fab fa-github-square"  style="font-size:3em; color:white;"></i>
+                    <i class="fab fa-github-square icons fa-3x"></i>
                 </a>
                 <p class="text-light">brianwachira</p>
             </div>
             <div class="col-md-2 col-sm text-center">
                 <a href="https://www.linkedin.com/in/brianwachira/">
-                    <i class="fab fa-linkedin" style="font-size:3em; color:white;"></i>
+                    <i class="fab fa-linkedin icons fa-3x"></i>
                 </a>
                 <p class="text-light">brianwachira</p>
             </div>
             <div class="col-md-2 col-sm text-center">
                 <a href="https://www.facebook.com/brianwizyy">
-                    <i class="fab fa-facebook-square" style="font-size:3em; color:white;"></i>
+                    <i class="fab fa-facebook-square icons fa-3x"></i>
                 </a>
                 <p class="text-light">Brian M. Wachira</p>
             </div>
             <div class="col-md-2 col-sm text-center">
                 <a href="https://www.instagram.com/mr_brianwachira/">
-                    <i class="fab fa-instagram" style="font-size:3em; color:white;"></i>
+                    <i class="fab fa-instagram icons fa-3x"></i>
                 </a>
                 <p class="text-light">mr_brianwachira</p>
             </div>
