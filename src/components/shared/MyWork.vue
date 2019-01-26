78b49a6601435cc1ea64cbfa61b76261a60c4ae5
brianwachira <brianwachira7@gmail.com>
2019-01-26 18:48:47 +0300

    reorder cards

--- src/components/shared/MyWork.vue
+++ src/components/shared/MyWork.vue
@@ -2,6 +2,16 @@
       <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
+              <div class="card bg-inherit mb-3"> 
+                <img class="card-img-top" src="../../assets/flappybird.jpg" alt="background3">
+                <div class="card-body ">
+                  <h5 class="card-title text-light font-weignt-bold">Flappy Bird</h5>
+                  <p class="card-text text-light">An android game based on flappybird concept from <a href="https://www.flappybird.io">flappybird.io.</a>Guide the bird as it adventures against human-made poles that have filled up the city.</p>
+                  <a href="https://github.com/brianwachira/flappybird-app" class="btn btn-inherit  text-center">View Project on GitHub</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
               <div class="card bg-inherit mb-3">
                 <img class="card-img-top" src="../../assets/avuvi.png" alt="background1">
                 <div class="card-body">
@@ -18,16 +28,6 @@
                   <h5 class="card-title text-light font-weignt-bold">Wahenga Walisema</h5>
                   <p class="card-text text-light">Share your wisdom to the rest of the world with this Vue App. Future implementations will have database intergration</p>
                   <a href="https://brianwachira.github.io/wahenga-walisema/dist/" class="btn btn-inherit">Visit Website</a><a href="https://github.com/brianwachira/wahenga-walisema" class="btn btn-inherit">View Project on GitHub</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card bg-inherit mb-3"> 
-                <img class="card-img-top" src="../../assets/flappybird.jpg" alt="background3">
-                <div class="card-body ">
-                  <h5 class="card-title text-light font-weignt-bold">Flappy Bird</h5>
-                  <p class="card-text text-light">An android game based on flappybird concept from <a href="https://www.flappybird.io">flappybird.io.</a>Guide the bird as it adventures against human-made poles that have filled up the city.</p>
-                  <a href="https://github.com/brianwachira/flappybird-app" class="btn btn-inherit  text-center">View Project on GitHub</a>
                 </div>
               </div>
             </div>
