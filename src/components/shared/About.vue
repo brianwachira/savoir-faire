5619692e2cf92b4d3ca2cf8416de208d4ae70684
brianwachira <brianwachira7@gmail.com>
2019-01-27 00:15:41 +0300

    remove font awesome

--- src/components/shared/About.vue
+++ src/components/shared/About.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container-fluid"> 
         <div class="row">
-            <div class="col px-3 px-md-0 text-center mt-5 text-light">
-                <p>You'll always find me going for unique trends,trying out different recipes in food..inshort always pursuing versatility. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit at architecto optio modi esse doloribus nostrum aliquid? Natus magni cum, dolorem impedit reprehenderit vitae a voluptate illo incidunt assumenda quisquam!</p>
-            </div>
+            <div class="col px-3 px-md-0 text-center mt-5 text-light w-50">
+                <p>I live in my mind alot because that's where everything goes down. You'll always find me going for unique trends(short for choosy),trying out different recipes in food Eg fried Ugali(see the definition of Ugali<a target="blank" href="https://www.google.com/search?q=ugali+definition&oq=Ugali+definition&aqs=chrome.0.0l6.4652j1j8&client=ubuntu&sourceid=chrome&ie=UTF-8"> here)</a>,background images in websites, shoelaces in shoes among others. Above that, I am a born again Christian. God loves me and you whether you have accepted Jesus or not. He just loves you unconditionally. God has enabled me to execute solutions through technology,which inturn advances His kingdom here on Earth.</p>
         </div>
+    </div>
     </div>
 </template>
