d80b9f176583e7ffddfb79f4abc79f1b39074b3c
brianwachira <brianwachira7@gmail.com>
2019-01-26 18:47:18 +0300

    set work content in cards

--- src/components/shared/MyWork.vue
+++ src/components/shared/MyWork.vue
@@ -5,9 +5,9 @@
               <div class="card bg-inherit mb-3">
                 <img class="card-img-top" src="../../assets/avuvi.png" alt="background1">
                 <div class="card-body">
-                  <h5 class="card-title text-light">Card title</h5>
-                  <p class="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-inherit">Go somewhere</a>
+                  <h5 class="card-title text-light font-weignt-bold">AVUVI PHOTOGRAPHY</h5>
+                  <p class="card-text text-light">Above showcases the minimum viable portfolio website(MVP) done for <a href="https://www.instagram.com/avuvi_photography/">Avuvi Photography</a>. Final release is on its way</p>
+                  <a href="https://brianwachira.github.io/avuvi-photography/html/index" class="btn btn-inherit">Visit Website</a><a href="https://github.com/brianwachira/avuvi-photography" class="btn btn-inherit">View source-code</a>
                 </div>
               </div>
             </div>
@@ -15,9 +15,9 @@
               <div class="card bg-inherit mb-3">
                 <img class="card-img-top" src="../../assets/wahenga-walisema.png" alt="background2">
                 <div class="card-body">
-                  <h5 class="card-title text-light">Card title</h5>
-                  <p class="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-inherit">Go somewhere</a>
+                  <h5 class="card-title text-light font-weignt-bold">Wahenga Walisema</h5>
+                  <p class="card-text text-light">Share your wisdom to the rest of the world with this Vue App. Future implementations will have database intergration</p>
+                  <a href="https://brianwachira.github.io/wahenga-walisema/dist/" class="btn btn-inherit">Visit Website</a><a href="https://github.com/brianwachira/wahenga-walisema" class="btn btn-inherit">View Project on GitHub</a>
                 </div>
               </div>
             </div>
@@ -25,28 +25,12 @@
               <div class="card bg-inherit mb-3"> 
                 <img class="card-img-top" src="../../assets/flappybird.jpg" alt="background3">
                 <div class="card-body ">
-                  <h5 class="card-title text-light">Card title</h5>
-                  <p class="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-inherit">Go somewhere</a>
+                  <h5 class="card-title text-light font-weignt-bold">Flappy Bird</h5>
+                  <p class="card-text text-light">An android game based on flappybird concept from <a href="https://www.flappybird.io">flappybird.io.</a>Guide the bird as it adventures against human-made poles that have filled up the city.</p>
+                  <a href="https://github.com/brianwachira/flappybird-app" class="btn btn-inherit  text-center">View Project on GitHub</a>
                 </div>
               </div>
             </div>
           </div>
       </div>  
 </template>
-<style scoped>
-.bg-inherit{
- background: inherit;
- border: 3px solid white;
-}
-.btn-inherit{
- background-color: inherit;
- color: white;
- opacity: 9;
- border: 3px solid rgb(255, 255, 255)
-}
-.btn-inherit:hover{
-    background-color: white;
-    color: #659999;
-}
-</style>
