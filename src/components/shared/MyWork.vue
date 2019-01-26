fad9be38350eb6c9fa493cab8726512b27620a00
brianwachira <brianwachira7@gmail.com>
2019-01-26 22:50:47 +0300

    set links target blank

--- src/components/shared/MyWork.vue
+++ src/components/shared/MyWork.vue
@@ -6,8 +6,8 @@
                 <img class="card-img-top" src="../../assets/flappybird.jpg" alt="background3">
                 <div class="card-body ">
                   <h5 class="card-title text-light font-weignt-bold">Flappy Bird</h5>
-                  <p class="card-text text-light">An android game based on flappybird concept from <a href="https://www.flappybird.io">flappybird.io.</a>Guide the bird as it adventures against human-made poles that have filled up the city.</p>
-                  <a href="https://github.com/brianwachira/flappybird-app" class="btn btn-inherit text-center ">View Project on GitHub</a>
+                  <p class="card-text text-light">An android game based on flappybird concept from <a href="https://www.flappybird.io" target="blank">flappybird.io.</a>Guide the bird as it adventures against human-made poles that have filled up the city.</p>
+                  <a href="https://github.com/brianwachira/flappybird-app" target="blank" class="btn btn-inherit text-center ">View Project on GitHub</a>
                 </div>
               </div>
             </div>
@@ -16,8 +16,8 @@
                 <img class="card-img-top" src="../../assets/avuvi.png" alt="background1">
                 <div class="card-body">
                   <h5 class="card-title text-light font-weignt-bold">AVUVI PHOTOGRAPHY</h5>
-                  <p class="card-text text-light">Above showcases the minimum viable portfolio website(MVP) done for <a href="https://www.instagram.com/avuvi_photography/">Avuvi Photography</a>. Final release is on its way</p>
-                  <a href="https://brianwachira.github.io/avuvi-photography/html/index" class="btn btn-inherit mr-0 mr-md-2 mr-lg-2 mr-xs-2 mb-2 mb-sm-0">Visit Website</a><a href="https://github.com/brianwachira/avuvi-photography" class="btn btn-inherit">View source-code</a>
+                  <p class="card-text text-light">Above showcases the minimum viable portfolio website(MVP) done for <a href="https://www.instagram.com/avuvi_photography/" target="blank">Avuvi Photography</a>. Final release is on its way</p>
+                  <a href="https://brianwachira.github.io/avuvi-photography/html/index" target="blank" class="btn btn-inherit mr-0 mr-md-2 mr-lg-2 mr-xs-2 mb-2 mb-sm-0">Visit Website</a><a href="https://github.com/brianwachira/avuvi-photography" target="blank" class="btn btn-inherit">View source-code</a>
                 </div>
               </div>
             </div>
@@ -27,7 +27,7 @@
                 <div class="card-body">
                   <h5 class="card-title text-light font-weignt-bold">Wahenga Walisema</h5>
                   <p class="card-text text-light">Share your wisdom to the rest of the world with this Vue App. Future implementations will have database intergration</p>
-                  <a href="https://brianwachira.github.io/wahenga-walisema/dist/" class="btn btn-inherit mr-0 mr-md-2 mr-lg-2 mr-xs-2 mb-2 mb-sm-0">Visit Website</a><a href="https://github.com/brianwachira/wahenga-walisema" class="btn btn-inherit">View Project on GitHub</a>
+                  <a href="https://brianwachira.github.io/wahenga-walisema/dist/" target="blank" class="btn btn-inherit mr-0 mr-md-2 mr-lg-2 mr-xs-2 mb-2 mb-sm-0">Visit Website</a><a href="https://github.com/brianwachira/wahenga-walisema"  target="blank" class="btn btn-inherit">View Project on GitHub</a>
                 </div>
               </div>
             </div>
