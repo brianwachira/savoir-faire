201adf40e488aa598c345e92d9121f40b5b2c273
brianwachira <brianwachira7@gmail.com>
2019-01-26 12:22:46 +0300

    add hr tag for styling

--- src/components/shared/ContactMe.vue
+++ src/components/shared/ContactMe.vue
@@ -1,37 +1,49 @@
 <template>
     <div class="container-fluid">
         <div class="row"> 
-            <div class="col-md-2 col-sm text-center"> 
+            <div class="col-md-2 col-sm text-center">
+                <hr style="width: 80%; border-color: white; border-width: 3px;"> 
                 <i class="fas fa-envelope icons fa-3x"></i>
                 <p class="text-light">brianwachira7@gmail.com</p>
-            </div>
-            <div class="col-md-2 col-sm text-center"> 
-                <i class="fab fa-whatsapp-square icons fa-3x"></i>
-                <p class="text-light">+254707250844</p>
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
             </div>
             <div class="col-md-2 col-sm text-center">
+                <hr style="width: 80%; border-color: white; border-width: 3px;"> 
+                <i class="fab fa-whatsapp-square icons fa-3x"></i>
+                <p class="text-light">+254707250844</p>
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
+            </div>
+            <div class="col-md-2 col-sm text-center">
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
                 <a href="https://www.github.com/brianwachira">
                     <i class="fab fa-github-square icons fa-3x"></i>
                 </a>
                 <p class="text-light">brianwachira</p>
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
             </div>
             <div class="col-md-2 col-sm text-center">
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
                 <a href="https://www.linkedin.com/in/brianwachira/">
                     <i class="fab fa-linkedin icons fa-3x"></i>
                 </a>
                 <p class="text-light">brianwachira</p>
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
             </div>
             <div class="col-md-2 col-sm text-center">
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
                 <a href="https://www.facebook.com/brianwizyy">
                     <i class="fab fa-facebook-square icons fa-3x"></i>
                 </a>
                 <p class="text-light">Brian M. Wachira</p>
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
             </div>
             <div class="col-md-2 col-sm text-center">
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
                 <a href="https://www.instagram.com/mr_brianwachira/">
                     <i class="fab fa-instagram icons fa-3x"></i>
                 </a>
                 <p class="text-light">mr_brianwachira</p>
+                <hr style="width: 80%; border-color: white; border-width: 3px;">
             </div>
         </div>
     </div>
