389d9abbea2296cebe9b40311e7b4cf0aad3b8f8
brianwachira <brianwachira7@gmail.com>
2019-01-26 20:18:21 +0300

    set nav justified

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -7,8 +7,8 @@
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
-                <ul class="navbar-nav mr-auto">
-                    <router-link  to="/" tag="li" class="nav-item nav-link" active-class="active" exact><a>Home</a></router-link>
+                <ul class="navbar-nav w-100 nav-justified">
+                    <router-link  to="/" tag="li" class="nav-item nav-link" active-class="active" exact><a class="text-light">Home</a></router-link>
                   
 
                     <router-link to="/skills" tag="li" class="nav-item nav-link" active-class="active"><a>My skills</a></router-link>
