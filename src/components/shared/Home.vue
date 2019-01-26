26f1d20f370362acedc88ea8a3dcda9fe4e8524d
brianwachira <brianwachira7@gmail.com>
2019-01-26 19:57:47 +0300

    remove whitespaces

--- src/components/shared/Home.vue
+++ src/components/shared/Home.vue
@@ -2,7 +2,6 @@
 <div class="content">
     <div class="container-fluid">
         <div class="row">  
-            
             <div class="col text-center mt-5 ">
                 <h1 class="text-light">Brian Wachira</h1>
                 <h2 class="text-light">Frontend/Android App Developer</h2>
@@ -13,16 +12,6 @@
 </div>    
 </template>
 <style scoped>
-.btn-inherit{
- background-color: inherit;
- color: white;
- opacity: 9;
- border: 3px solid rgb(255, 255, 255)
-}
-.btn-inherit:hover{
-    background-color: white;
-    color: #659999;
-}
 .mt-5{
     margin-top: 4.5rem!important;
 }
