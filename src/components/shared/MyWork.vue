c4558ed9df7e4f6628de364fae6a71e4d4eada41
brianwachira <brianwachira7@gmail.com>
2019-01-26 18:12:06 +0300

    redesign card

--- src/components/shared/MyWork.vue
+++ src/components/shared/MyWork.vue
@@ -2,32 +2,32 @@
       <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
-              <div class="card">
+              <div class="card bg-inherit mb-3">
                 <img class="card-img-top" src="../../assets/avuvi.png" alt="background1">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <h5 class="card-title text-light">Card title</h5>
+                  <p class="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-inherit">Go somewhere</a>
                 </div>
               </div>
             </div>
             <div class="col-md-4">
-              <div class="card">
+              <div class="card bg-inherit mb-3">
                 <img class="card-img-top" src="../../assets/wahenga-walisema.png" alt="background2">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <h5 class="card-title text-light">Card title</h5>
+                  <p class="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-inherit">Go somewhere</a>
                 </div>
               </div>
             </div>
             <div class="col-md-4">
-              <div class="card">
+              <div class="card bg-inherit mb-3"> 
                 <img class="card-img-top" src="../../assets/flappybird.jpg" alt="background3">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="card-body ">
+                  <h5 class="card-title text-light">Card title</h5>
+                  <p class="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-inherit">Go somewhere</a>
                 </div>
               </div>
             </div>
@@ -38,5 +38,15 @@
 .bg-inherit{
  background: inherit;
  border: 3px solid white;
+}
+.btn-inherit{
+ background-color: inherit;
+ color: white;
+ opacity: 9;
+ border: 3px solid rgb(255, 255, 255)
+}
+.btn-inherit:hover{
+    background-color: white;
+    color: #659999;
 }
 </style>
