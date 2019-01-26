d2c89015a158df715dc928f75f407ed10cf7f02a
brianwachira <brianwachira7@gmail.com>
2019-01-26 09:30:33 +0300

    set styling for button

--- src/components/shared/Home.vue
+++ src/components/shared/Home.vue
@@ -1,13 +1,29 @@
 <template>
 <div class="content">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col text-center mt-4">
-                <h1>Brian Wachira</h1>
-                <h2>Frontend/Android App Developer</h2>
-                <router-link class="btn btn-light mt-4" to="/about" tag="button">About Me</router-link>
+        <div class="row">  
+            
+            <div class="col text-center mt-5 ">
+                <h1 class="text-light">Brian Wachira</h1>
+                <h2 class="text-light">Frontend/Android App Developer</h2>
+                <router-link class="btn btn-inherit mt-5" to="/about" tag="button">About Me</router-link>
             </div>
         </div>
     </div>
 </div>    
 </template>
+<style scoped>
+.btn-inherit{
+ background-color: inherit;
+ color: white;
+ opacity: 9;
+ border: 3px solid rgb(255, 255, 255)
+}
+.btn-inherit:hover{
+    background-color: white;
+    color: #659999;
+}
+.mt-5{
+    margin-top: 4.5rem!important;
+}
+</style>
