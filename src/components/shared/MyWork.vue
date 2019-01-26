80e525250c0c6d18cceb79702311875f3ba98caf
brianwachira <brianwachira7@gmail.com>
2019-01-26 18:04:36 +0300

    add background inherit class

--- src/components/shared/MyWork.vue
+++ src/components/shared/MyWork.vue
@@ -29,20 +29,14 @@
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-    <div class="carousel-item">
-      <img src="../../assets/flappybird.jpg" class="d-block w-100" alt="background2">
-    </div>
-    <div class="carousel-item">
-      <img src="../../assets/wahenga-walisema.png" class="d-block w-100" alt="background3">
+              </div>
             </div>
           </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+      </div>  
 </template>
+<style scoped>
+.bg-inherit{
+ background: inherit;
+ border: 3px solid white;
+}
+</style>
