c53695c191f3226c970e8c842eff6bd8c8c7f0ca
brianwachira <brianwachira7@gmail.com>
2019-01-26 11:51:31 +0300

    refactor icons to their col-md-2

--- src/components/shared/ContactMe.vue
+++ src/components/shared/ContactMe.vue
@@ -1,23 +1,33 @@
 <template>
-    <p>Contact me</p>
-        <div class="row"> .
-            <div class="col text-center">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 col-sm text-center"> 
                 <i class="fas fa-envelope" style="font-size: 3em; color: white;"></i>
                 <p class="text-light">brianwachira7@gmail.com</p>
+            </div>
+            <div class="col-md-2 col-sm text-center"> 
                 <i class="fab fa-whatsapp-square" style="font-size:3em; color:white;"></i>
                 <p class="text-light">+254707250844</p>
+            </div>
+            <div class="col-md-2 col-sm text-center">
                 <a href="https://www.github.com/brianwachira">
-                <i class="fab fa-github-square"  style="font-size:3em; color:white;"></i>
+                    <i class="fab fa-github-square"  style="font-size:3em; color:white;"></i>
                 </a>
                 <p class="text-light">brianwachira</p>
+            </div>
+            <div class="col-md-2 col-sm text-center">
                 <a href="https://www.linkedin.com/in/brianwachira/">
                     <i class="fab fa-linkedin" style="font-size:3em; color:white;"></i>
                 </a>
                 <p class="text-light">brianwachira</p>
+            </div>
+            <div class="col-md-2 col-sm text-center">
                 <a href="https://www.facebook.com/brianwizyy">
                     <i class="fab fa-facebook-square" style="font-size:3em; color:white;"></i>
                 </a>
                 <p class="text-light">Brian M. Wachira</p>
+            </div>
+            <div class="col-md-2 col-sm text-center">
                 <a href="https://www.instagram.com/mr_brianwachira/">
                     <i class="fab fa-instagram" style="font-size:3em; color:white;"></i>
                 </a>
