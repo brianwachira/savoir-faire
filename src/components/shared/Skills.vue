68b88529d74a7d62b4588b3ff1d94d66ae5afb30
brianwachira <brianwachira7@gmail.com>
2019-01-27 17:18:03 +0300

    set margin bottom 3

--- src/components/shared/Skills.vue
+++ src/components/shared/Skills.vue
@@ -2,38 +2,38 @@
     <div class="container-fluid">
         <div class="row"> 
             <div class="col-md-6 offset-md-2 col-md-5  offset-md-3 col-sm-12 ">
-                <div class="h2 heading text-light text-center font-weight-bolder">Tools I use mostly</div>
+                <div class="h2 heading text-light text-center font-weight-bolder mb-sm-5 mb-md-5 mb-lg-5 mb-xl-5 mb-5">Tools I use mostly. I call them tools because they help me get a job done</div>
                 <label for="android" class="text-light font-weight-bolder">Android Native App Development</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar bg-success" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <label for="Vue.js" class="text-light font-weight-bolder">Vue</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar bg-vue" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <label for="Bootstrap" class="text-light font-weight-bolder">Bootstrap</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <label for="HTML5" class="text-light font-weight-bolder">HTML5</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar bg-orange" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <label for="CSS 3" class="text-light font-weight-bolder">CSS 3</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar bg-cssblue" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <label for="JQuery" class="text-light font-weight-bolder">JQuery</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar bg-jblue" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <label for="API" class="text-light font-weight-bolder">APIs(openweather API,hackernews API, googlemaps API,mailchimp API)</label>
-                <div class="progress">
+                <div class="progress mb-3">
                     <div class="progress-bar bg-danger" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             <!--/div> 
             <div class="col-md-6 col-sm-12"-->
-                <h2 class="header text-light text-center font-weight-bolder mt-5">Other Tools</h2>
+                <h2 class="header text-light text-center font-weight-bolder mt-5">Other Tools I Know</h2>
                 <label for="C-programming" class="text-light font-weight-bolder">C programming</label>
                 <div class="progress">
                     <div class="progress-bar bg-vue" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
