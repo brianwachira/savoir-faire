fbda13309b4af24951de030a7af1eb4bdef7140a
brianwachira <brianwachira7@gmail.com>
2019-01-28 01:03:35 +0300

    add / for routes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Skills from './components/shared/Skills.vue'
 import myWork from './components/shared/MyWork.vue'
 import contactMe from './components/shared/ContactMe.vue'
 export const routes = [
-    {path:'',component:Home},
+    {path:'/',component:Home},
     {path:'/skills',component:Skills},
     {path:'/work',component:myWork},
     {path:'/contact',component:contactMe},
