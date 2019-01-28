d6d73b4ad67a022b73953978db74fc9586ba9901
brianwachira <brianwachira7@gmail.com>
2019-01-28 12:55:52 +0300

    format about me content

--- src/components/shared/About.vue
+++ src/components/shared/About.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container-fluid"> 
-        <div class="row">
-            <div class="col-md-6 offset-md-3 col-sm offset-sm px-3 px-md-0 text-center  text-light  mt-0 mt-sm-5 ubuntu">
-                <p>What you saw on Home is part of my work titles. I am a thinker but above<br/> all a born again christian full of creativity. Some of my hobbies include 
+        <div class="row"> 
+            <div class="col-md-6 offset-md-2 col-md-5  offset-md-3 col-sm-12 justify-content-center  text-light  mt-0 mt-sm-5 ubuntu">
+                <p>What you saw on Home is kinda like man-made work titles, basically a drop in the ocean. Oh and by the way, savoir faire is a distant synonym to portfolio or resume. Yeah don't expect me to use a common name like everyone does. That said, I am a thinker but above all a born again christian full of creativity. Some of my hobbies include 
                     <ul class="mt-0 mt-sm-4">
                         <li>Watching movies</li>
                         <li>Jogging especially evening jogs</li>
                         <li>reading self-improvement books</li>
                         <li class="mb-0 mb-sm-4">cooking</li>
                     </ul>
-                     I can offer leadership and guidance when need arises,fit in a team and try to <br/> relate with each person's point of view. Stick around and you will learn more about me.     
+                     I love animation clips because of the almost endless possibilities portrayed by them.<br/>Now to the serious stuff, I can offer leadership and guidance when need arises,fit in a team and try to relate with each person's point of view. Stick around and you will learn more about me.     
                 </p>
-        </div>
-    </div>
+            </div>
+         </div>
     </div>
 </template>
 <style scoped>
