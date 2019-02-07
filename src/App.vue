0ae3110417c29d6f95ef7b9eced23acebae055a6
brianwachira <brianwachira7@gmail.com>
2019-02-07 12:00:20 +0300

    garbage out unused styles

--- src/App.vue
+++ src/App.vue
@@ -29,19 +29,3 @@ export default {
   }
 }
 </script>
-
-<style>
-/*.background-img{
- /*background-image: url('./assets/background2.jpg'); 
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;*/
-  /*background: #659999;  /* fallback for old browsers */
-  /*background: -webkit-linear-gradient(to right, #f4791f, #659999);  /* Chrome 10-25, Safari 5.1-6 */
-  /*background: linear-gradient(to right, #f4791f, #659999); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-  /*height: 100%;
-  
-}
-*/
-</style>
